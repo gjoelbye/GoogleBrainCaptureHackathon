66a9547f5674dcaf29ed8e04e5d9cab801d31bef
William Theodor <willehn@gmail.com>
2024-03-12 21:04:28 +0100

    braincapture bhutan v4.1

--- src/data/conf/eeg_annotations.py
+++ src/data/conf/eeg_annotations.py
@@ -1,5 +1,5 @@
-braincapture_annotations = {'Eye blinking': 0, 'Eye movement left-right': 1, 'Eyes closed': 2, 
-                            'Eyes opened': 3, 'Jaw clenching': 4, 'Hyperventilation': 5}
+#braincapture_annotations = {'Eye blinking': 0, 'Eye movement left-right': 1, 'Eyes closed': 2, 'Eyes opened': 3, 'Jaw clenching': 4, 'Hyperventilation': 5}
+braincapture_annotations = {'Eye blinking': 0, 'Eye movement left-right': 1, 'Eyes closed': 2, 'Eyes opened': 3, 'Jaw clenching': 4}
 
 tuh_eeg_artefact_annotations = {'eyem': 0, 'chew': 1, 'musc': 2, 'elec': 3}
 
